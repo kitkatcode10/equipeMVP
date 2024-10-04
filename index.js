0fa2ee08e9fefa2f19f91a3488de06ebc72df9ca
kitkatcode <katherinekayanwong@outlook.com>
2024-10-04 14:24:17 +0100

    MVP code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import '@react-native-firebase/app';
-import '@react-native-firebase/auth';
+import auth from '@react-native-firebase/auth';
 import '@react-native-firebase/firestore';
 
 import { initializeApp } from '@react-native-firebase/app';
